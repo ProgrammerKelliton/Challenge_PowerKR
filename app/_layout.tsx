0f45e3979e840a91bfef011c2aa665effe66ac11
Kelliton Santana <programmerKelliton@gmail.com>
2024-01-31 17:46:55 -0300

    initial

---
+++ app/_layout.tsx
@@ -0,0 +1,55 @@
+import '../tamagui.css';
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { SplashScreen, Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import { TamaguiProvider } from 'tamagui';
+
+import { config } from '../tamagui.config';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+export { ErrorBoundary } from 'expo-router';
+
+export const unstable_settings = {
+  initialRouteName: 'index',
+};
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [interLoaded, interError] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (interLoaded || interError) {
+      SplashScreen.hideAsync();
+    }
+  }, [interLoaded, interError]);
+
+  if (!interLoaded && !interError) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <TamaguiProvider config={config} defaultTheme={colorScheme as any}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name='index' options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
